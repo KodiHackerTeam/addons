2cd1402b9a67b7cb72e8c72ebb55b49cccd1e935
sicaronaldo <sicaronaldo@users.noreply.github.com>
2019-01-23 14:07:17 +0100

    Initial commit

---
+++ catoal/Addons Peliculas, Series y Documentales/Palantir/Solo si creaste widget/autoexec.py
@@ -0,0 +1,5 @@
+#Script por Taox Trece para Taox Wizard
+import xbmc
+xbmc.executebuiltin("RunAddon(plugin.video.palantir)", True)
+xbmc.executebuiltin("ActivateWindow(home)", True)
+xbmc.executebuiltin("ReloadSkin()", True)
